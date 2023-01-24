ab2a82aa404d1c7b2ceb9797f6a08125f4b0f3e3
danielfgc <daniel.fernandez@campusfp.es>
2023-01-24 12:46:55 +0100

    connected with firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
+import { firebaseConfig } from './database/credentials';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
---
+++ src/app/database/credentials.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAYSHastanfySax3o8YdgMMj2Q4WuQ9sBE",
+  authDomain: "ionic-app-5985e.firebaseapp.com",
+  projectId: "ionic-app-5985e",
+  storageBucket: "ionic-app-5985e.appspot.com",
+  messagingSenderId: "305034574088",
+  appId: "1:305034574088:web:98938d6c8db0584e9b94bd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
