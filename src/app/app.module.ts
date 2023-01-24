8ab78bb97ba88412764505377423eeb381ab28c0
fernandotrujillo95 <fernando.trujillo@campusfp.es>
2023-01-24 14:40:36 +0100

    Cambios en HomePage, que se tienen que pasar al componente de formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { firebaseConfig } from './database/credentials';
-import { AppComponent } from './app.component';
+/* import { AngularFireModule } from 'angularfire2';
+ *//* import { AngularFirestoreModule } from 'angularfire2/firestore';
+ */import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+/* import { firebaseConfig } from './database/credentials';
+ */import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,AngularFireModule.initializeApp(firebaseConfig),
-    AngularFirestoreModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
