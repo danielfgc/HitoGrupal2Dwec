89b0b125e2d7144effc4cf25d9765c9e73f55079
danielfgc <daniel.fernandez@campusfp.es>
2023-01-24 12:13:54 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'hitogrupal2dwec',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
