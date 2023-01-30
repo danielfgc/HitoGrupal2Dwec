e3dc55c0322a5bf39d0a7381990249bcf1b60c59
albertocarlos.diaz@campusfp.es <albertocarlos.diaz@campusfp.es>
2023-01-30 02:42:15 +0100

    pequeñas mejoras en home page. falta que el boton abra el formulario

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
+import {FormularioComponent} from '../formulario/formulario.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePage,
+  },
+  {
+    path: 'form',
+    component: FormularioComponent,
   }
 ];
 
